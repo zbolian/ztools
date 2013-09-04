2ca0640192138fb35e7312000c3e47e1a0eef0ef
Zach Bolian <zach.bolian@bazaarvoice.com>
2013-09-04 17:44:23 -0500

    Added zmail for emailing. Currently only one method to email through Office 365 with a file attached.

---
+++ zmail.py
@@ -0,0 +1,35 @@
+
+import smtplib, os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.utils import COMMASPACE, formatdate
+from email import encoders
+
+def send_mail(uid, pwd, send_from, send_to, subject, text, files=[], server="localhost"):
+    assert type(send_to)==list
+    assert type(files)==list
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg.attach( MIMEText(text) )
+
+    for f in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(f,"rb").read() )
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP()
+    smtp.connect(server,587)
+    smtp.starttls()
+    smtp.login(uid, pwd)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+
+
+__author__ = 'zach.bolian'
